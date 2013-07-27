280ca9c5e725e979acbde678dad4908329a4a843
Amanda <amanda.n.krauss@gmail.com>
2013-07-27 13:41:08 -0500

    move script

--- includes/header.php
+++ includes/header.php
@@ -2,20 +2,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Amanda Krauss -- Austin Front-End and Wordpress Development, UX/UI Design, Etc.</title>        
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-7962943-7']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>      
-	
 <link rel="stylesheet" href="js/flexslider.css" type="text/css">
 <link rel ="stylesheet" href="js/fancybox/source/jquery.fancybox.css"></script>  
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>       
@@ -33,19 +20,6 @@
 <!--[if lt IE 9]><script src="scripts/selectivizr.js"></script>  <![endif]-->  <!-- Adds CSS3 to IE6/7/8 !-->
 <!--[if lt IE 9]><script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script><![endif]-->  <!-- Adds media queries to IE6/7/8 !-->  
 
-                     
-<script>
-$(window).load(function() {
-  $('#homeslider').flexslider({
-    animation: "slide",
-	useCSS: true,
-	manualControls: "#pagenav li a.page-nav",
-	slideshow: false,
-	controlNav: true,              
-    directionNav: false             
-  });                    
-}); 
-</script>
 </head>
 
 <body>
